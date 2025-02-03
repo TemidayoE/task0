bae9928e07d89bc0e6fc216a1d41977f214adbb6
Temidayo Ejide <temidayoejide1@gmail.com>
2025-02-03 13:36:26 +0100

    initial commit

--- basic_app/views.py
+++ basic_app/views.py
@@ -4,9 +4,9 @@ from django.utils.timezone import now
 
 def details(request):
   data = {
-    "slack_email": "temidayoejide1@gmail.com",
+    "email": "temidayoejide1@gmail.com",
     "current_datetime": now().isoformat(),
-    "git_repo": "https://github.com/TemidayoE/task0"
+    "github_url": "https://github.com/TemidayoE/task0"
   }
   
   return JsonResponse(data)
